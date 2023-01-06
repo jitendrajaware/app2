d02c52e5aba46ee017688298c7d5768042c95f59
Jitendra Jaware <jitendra.jaware@alepo.com>
2023-01-06 12:01:05 +0530

    initial commit

---
+++ src/main/java/com/jittech/app2/constants/App2PortletKeys.java
@@ -0,0 +1,11 @@
+package com.jittech.app2.constants;
+
+/**
+ * @author jitendra.jaware
+ */
+public class App2PortletKeys {
+
+	public static final String APP2 =
+		"com_jittech_app2_App2Portlet";
+
+}
---
+++ src/main/java/com/jittech/app2/portlet/App2Portlet.java
@@ -0,0 +1,30 @@
+package com.jittech.app2.portlet;
+
+import com.jittech.app2.constants.App2PortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author jitendra.jaware
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=App2",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + App2PortletKeys.APP2,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class App2Portlet extends MVCPortlet {
+}
